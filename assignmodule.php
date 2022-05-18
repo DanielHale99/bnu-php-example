d1e9cf9851d276b3a57fed3da7f88c875b6ba9dd
Daniel Hale <72168379+DanielHale99@users.noreply.github.com>
2022-05-18 07:09:23 +0100

    updates

--- assignmodule.php
+++ assignmodule.php
@@ -1,48 +1,64 @@
 <?php
+	
 
-include("_includes/config.inc");
-include("_includes/dbconnect.inc");
-include("_includes/functions.inc");
+	include("_includes/config.inc");
+	include("_includes/dbconnect.inc");
+	include("_includes/functions.inc");
+	
 
+	
 
-// check logged in
-if (isset($_SESSION['id'])) {
+	// check logged in
+	if (isset($_SESSION['id'])) {
+	
 
-   echo template("templates/partials/header.php");
-   echo template("templates/partials/nav.php");
+	   echo template("templates/partials/header.php");
+	   echo template("templates/partials/nav.php");
+	
 
-   // If a module has been selected
-   if (isset($_POST['selmodule'])) {
-      $sql = "insert into studentmodules values ('" .  $_SESSION['id'] . "','" . $_POST['selmodule'] . "');";
-      $result = mysqli_query($conn, $sql);
-      $data['content'] .= "<p>The module " . $_POST['selmodule'] . " has been assigned to you</p>";
-   }
-   else  // If a module has not been selected
-   {
+	   // If a module has been selected
+	   if (isset($_POST['selmodule'])) {
+	
 
-     // Build sql statment that selects all the modules
-     $sql = "select * from module";
-     $result = mysqli_query($conn, $sql);
+	      // Prepare statement and bind values
+	      $stmt = $conn->prepare("INSERT INTO studentmodules VALUES (?, ?);");
+	      $stmt->bind_param("ss", $_SESSION['id'], $_POST['selmodule']);
+	      $stmt->execute();
+	      $result = $stmt->get_result();
+	
 
-     $data['content'] .= "<form name='frmassignmodule' action='' method='post' >";
-     $data['content'] .= "Select a module to assign<br/>";
-     $data['content'] .= "<select name='selmodule' >";
-     // Display the module name sin a drop down selection box
-     while($row = mysqli_fetch_array($result)) {
-        $data['content'] .= "<option value='$row[modulecode]'>$row[name]</option>";
-     }
-     $data['content'] .= "</select><br/>";
-     $data['content'] .= "<input type='submit' name='confirm' value='Save' />";
-     $data['content'] .= "</form>";
-   }
+	      $data['content'] .= "<p>The module " . $_POST['selmodule'] . " has been assigned to you</p>";
+	   }
+	   else 
+	   {
+	      
+	      $sql = "select * from module";
+	      $result = mysqli_query($conn, $sql);
+	
 
-   // render the template
-   echo template("templates/default.php", $data);
+	      $data['content'] .= "<form name='frmassignmodule' action='' method='post' >";
+	      $data['content'] .= "<label for='selmodule' class='form-label'>Select a module to assign:</label><br/>";
+	      $data['content'] .= "<select class='form-select' style='width:20%;' name='selmodule' >";
+	      // Display the module name sin a drop down selection box
+	      while($row = mysqli_fetch_array($result)) {
+	         $data['content'] .= "<option value='$row[modulecode]'>$row[name]</option>";
+	      }
+	      $data['content'] .= "</select><br/>";
+	      $data['content'] .= "<input type='submit' class='btn btn-primary' name='confirm' value='Save' />";
+	      $data['content'] .= "</form>";
+	   }
+	
 
-} else {
-   header("Location: index.php");
-}
+	   // render the template
+	   echo template("templates/default.php", $data);
+	
 
-echo template("templates/partials/footer.php");
+	} else {
+	   header("Location: index.php");
+	}
+	
 
-?>
+	echo template("templates/partials/footer.php");
+	
+
+	?>
